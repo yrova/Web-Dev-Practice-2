499e12d3e3bbe35d588d8532450249f375c8d490
yrova <avory.richie@gmail.com>
2020-01-06 14:30:53 -0500

    edited

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,57 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => {
-  //save clicks of each button to own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const handleGoodClick = newValue => {
-    setGood(newValue);
-  };
-
-  const handleNeutralClick = newValue => {
-    setNeutral(newValue);
-  };
-
-  const handleBadClick = newValue => {
-    setBad(newValue);
-  };
-
-  return (
-    <div>
-      <div>
-        <h1>give feedback</h1>
-        <Button handleClick={() => handleGoodClick(good + 1)} text='good' />
-        <Button
-          handleClick={() => handleNeutralClick(neutral + 1)}
-          text='neutral'
-        />
-        <Button handleClick={() => handleBadClick(bad + 1)} text='bad' />
-      </div>
-      <div>
-        <p>good {good}</p>
-      </div>
-      <Statistics values={good} />
-    </div>
-  );
-};
-
-const Statistics = props => {
-  const [all, setAll] = useState(0);
-  console.log(props.values);
-
-  return (
-    <div>
-      <h1>statistics</h1>
-      <p>good {props.values} </p>
-    </div>
-  );
-};
-
-const Button = props => (
-  <button onClick={props.handleClick}> {props.text}</button>
-);
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
