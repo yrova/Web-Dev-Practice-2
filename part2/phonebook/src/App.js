f3bcc3c3b1bcad60e0c867172c1e3f959b147b2d
yrova <avory.richie@gmail.com>
2020-01-09 18:54:32 -0500

    uses json server to update initial

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import FilterNumbers from "./components/FilterNumbers";
+import AddEntry from "./components/AddEntry";
+import DisplayNumbers from "./components/DisplayNumbers";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 }
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [filterBy, setNewFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then(response => {
+      setPersons(response.data);
+    });
+  }, []);
 
   return (
     <div>
